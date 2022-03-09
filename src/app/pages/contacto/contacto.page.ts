66a20c068533480d071a08a9490dbd7e31bd7f37
Adrian <egeaadrian14@gmail.com>
2022-03-09 21:29:11 +0100

    finalizado contacto, eventos y algunos estilos

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoPage implements OnInit {
 
+  score: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+
+    
   }
+
+
 
 }
